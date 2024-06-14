011e1aa19171aacd257e0eecfaf721c23b588d69
Ark <arkarmoemyint100@gmail.com>
2024-06-13 23:16:11 -0400

    Card Structure

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -32,3 +32,71 @@ export async function createThread({text, author, communityId, path}: Params){
         throw new Error(`Error creating thread: ${error.message}`)
     }
 };
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20){
+    connectToDB();
+
+    // Calculate the number of posts to skip
+    const skipAmount = (pageNumber - 1) * pageSize;
+    //Fetch the posts that have no parents (top-level threads)
+    const postsQuery = Thread.find({parentId: { $in: [null, undefined]}})
+        .sort({ createdAt: 'desc' })
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({ path: "author", model: User})
+        .populate({ 
+            path: 'children', 
+            populate: {
+                path: 'author',
+                model: User,
+                select: '_id name parentId image'
+            }
+        })
+        const totalPostsCount = await Thread.countDocuments({parentId: { $in: [null, undefined]}})
+
+        const posts = await postsQuery.exec();
+
+        const isNext = totalPostsCount > skipAmount + posts.length;
+
+        return { posts, isNext };
+}
+
+export async function fetchThreadById(id: string) {
+    connectToDB();
+  
+    try {
+
+      // TODO: Populate Community
+      const thread = await Thread.findById(id)
+        .populate({
+          path: "author",
+          model: User,
+          select: "_id id name image",
+        }) // Populate the author field with _id and username
+        .populate({
+          path: "children", // Populate the children field
+          populate: [
+            {
+              path: "author", // Populate the author field within children
+              model: User,
+              select: "_id id name parentId image", // Select only _id and username fields of the author
+            },
+            {
+              path: "children", // Populate the children field within children
+              model: Thread, // The model of the nested children (assuming it's the same "Thread" model)
+              populate: {
+                path: "author", // Populate the author field within nested children
+                model: User,
+                select: "_id id name parentId image", // Select only _id and username fields of the author
+              },
+            },
+          ],
+        })
+        .exec();
+  
+      return thread;
+    } catch (error: any) {
+        //console.error("Error while fetching thread:", err);
+        throw new Error(`Error while fetching thread: ${error.message}`);
+      }
+    }
--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      serverComponentsExternalPackages: ["mongoose"],
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "img.clerk.com",
-        },
-        {
-          protocol: "https",
-          hostname: "images.clerk.dev",
-        },
-        {
-          protocol: "https",
-          hostname: "uploadthing.com",
-        },
-        {
-          protocol: "https",
-          hostname: "placehold.co",
-        },
-      ],
-    },
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-  };
-  
-  export default nextConfig;
-  
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
