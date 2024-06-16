bd1d22ea6b1c99375a6e02229a86626d1c5f65ac
Ark <arkarmoemyint100@gmail.com>
2024-06-16 16:21:30 -0400

    maxDuration

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -206,3 +206,12 @@ export const POST = async (request: Request) => {
     }
   }
 };
+
+export const maxDuration = 5; // This function can run for a maximum of 5 seconds
+export const dynamic = 'force-dynamic';
+ 
+export function GET(request: Request) {
+  return new Response('Vercel', {
+    status: 200,
+  });
+}
